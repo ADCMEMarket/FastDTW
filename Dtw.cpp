5ab359d2be3592958d43aa639d85ef9a19374f19
Kailai Xu <kailaix@stanford.edu>
2020-04-21 11:40:08 -0700

    update dtw

--- Dtw.cpp
+++ Dtw.cpp
@@ -109,6 +109,7 @@ public:
     const Tensor& p = context->input(2);
     const Tensor& s = context->input(3);
     const Tensor& t = context->input(4);
+    const Tensor& fast = context->input(5);
     
     
     const TensorShape& grad_l_shape = grad_l.shape();
@@ -116,6 +117,7 @@ public:
     const TensorShape& p_shape = p.shape();
     const TensorShape& s_shape = s.shape();
     const TensorShape& t_shape = t.shape();
+    const TensorShape& fast_shape = fast.shape();
     
     
     DCHECK_EQ(grad_l_shape.dims(), 0);
@@ -131,6 +133,7 @@ public:
     
     TensorShape grad_s_shape(s_shape);
     TensorShape grad_t_shape(t_shape);
+    TeensorShape grad_fast_shape(fast_shape);
 
     int n1 = s_shape.dim_size(0), n2 = t_shape.dim_size(0), np = p_shape.dim_size(0);
             
@@ -140,6 +143,8 @@ public:
     OP_REQUIRES_OK(context, context->allocate_output(0, grad_s_shape, &grad_s));
     Tensor* grad_t = NULL;
     OP_REQUIRES_OK(context, context->allocate_output(1, grad_t_shape, &grad_t));
+    Tensor* grad_fast = NULL;
+    OP_REQUIRES_OK(context, context->allocate_output(2, grad_fast_shape, &grad_fast));
     
     // get the corresponding Eigen tensors for data access
     
@@ -161,131 +166,3 @@ public:
 };
 REGISTER_KERNEL_BUILDER(Name("DtwGrad").Device(DEVICE_CPU), DtwGradOp);
 
-
-/***************************************************************************************
-**********************            GPU Operator            ******************************
-***************************************************************************************/
-
-
-#ifdef USE_GPU
-class DtwOpGPU : public OpKernel {
-private:
-  
-public:
-  explicit DtwOpGPU(OpKernelConstruction* context) : OpKernel(context) {
-
-  }
-
-  void Compute(OpKernelContext* context) override {    
-    DCHECK_EQ(2, context->num_inputs());
-    
-    
-    const Tensor& s = context->input(0);
-    const Tensor& t = context->input(1);
-    
-    
-    const TensorShape& s_shape = s.shape();
-    const TensorShape& t_shape = t.shape();
-    
-    
-    DCHECK_EQ(s_shape.dims(), 1);
-    DCHECK_EQ(t_shape.dims(), 1);
-
-    // extra check
-        
-    // create output shape
-    
-    TensorShape l_shape({});
-    TensorShape p_shape({-1,2});
-            
-    // create output tensor
-    
-    Tensor* l = NULL;
-    OP_REQUIRES_OK(context, context->allocate_output(0, l_shape, &l));
-    Tensor* p = NULL;
-    OP_REQUIRES_OK(context, context->allocate_output(1, p_shape, &p));
-    
-    // get the corresponding Eigen tensors for data access
-    
-    auto s_tensor = s.flat<double>().data();
-    auto t_tensor = t.flat<double>().data();
-    auto l_tensor = l->flat<double>().data();
-    auto p_tensor = p->flat<int32>().data();   
-
-    // implement your forward function here 
-
-    // TODO:
-
-  }
-};
-REGISTER_KERNEL_BUILDER(Name("Dtw").Device(DEVICE_GPU), DtwOpGPU);
-
-class DtwGradOpGPU : public OpKernel {
-private:
-  
-public:
-  explicit DtwGradOpGPU(OpKernelConstruction* context) : OpKernel(context) {
-    
-  }
-  
-  void Compute(OpKernelContext* context) override {
-    
-    
-    const Tensor& grad_l = context->input(0);
-    const Tensor& grad_p = context->input(1);
-    const Tensor& l = context->input(2);
-    const Tensor& p = context->input(3);
-    const Tensor& s = context->input(4);
-    const Tensor& t = context->input(5);
-    
-    
-    const TensorShape& grad_l_shape = grad_l.shape();
-    const TensorShape& grad_p_shape = grad_p.shape();
-    const TensorShape& l_shape = l.shape();
-    const TensorShape& p_shape = p.shape();
-    const TensorShape& s_shape = s.shape();
-    const TensorShape& t_shape = t.shape();
-    
-    
-    DCHECK_EQ(grad_l_shape.dims(), 0);
-    DCHECK_EQ(grad_p_shape.dims(), 2);
-    DCHECK_EQ(l_shape.dims(), 0);
-    DCHECK_EQ(p_shape.dims(), 2);
-    DCHECK_EQ(s_shape.dims(), 1);
-    DCHECK_EQ(t_shape.dims(), 1);
-
-    // extra check
-    // int m = Example.dim_size(0);
-        
-    // create output shape
-    
-    TensorShape grad_s_shape(s_shape);
-    TensorShape grad_t_shape(t_shape);
-            
-    // create output tensor
-    
-    Tensor* grad_s = NULL;
-    OP_REQUIRES_OK(context, context->allocate_output(0, grad_s_shape, &grad_s));
-    Tensor* grad_t = NULL;
-    OP_REQUIRES_OK(context, context->allocate_output(1, grad_t_shape, &grad_t));
-    
-    // get the corresponding Eigen tensors for data access
-    
-    auto s_tensor = s.flat<double>().data();
-    auto t_tensor = t.flat<double>().data();
-    auto grad_l_tensor = grad_l.flat<double>().data();
-    auto grad_p_tensor = grad_p.flat<int32>().data();
-    auto l_tensor = l.flat<double>().data();
-    auto p_tensor = p.flat<int32>().data();
-    auto grad_s_tensor = grad_s->flat<double>().data();
-    auto grad_t_tensor = grad_t->flat<double>().data();   
-
-    // implement your backward function here 
-
-    // TODO:
-    
-  }
-};
-REGISTER_KERNEL_BUILDER(Name("DtwGrad").Device(DEVICE_GPU), DtwGradOpGPU);
-
-#endif
